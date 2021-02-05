c8de395397b8b645335ae7da6bca162519f0e41e
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-02-05 10:14:35 -0700

    login form

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+
+import AdminLogin from "./font-end/admin-login"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <BrowserRouter>
+          <Switch>
+            <Route path ="/login" component={AdminLogin} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
