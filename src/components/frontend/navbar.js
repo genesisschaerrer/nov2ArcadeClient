bc00fb6f543037ea07f7ae54910adf3fa6d41e3f
bradenslsb <bbir1199@gmail.com>
2021-02-05 09:55:21 -0700

    This is the frontend set up

---
+++ src/components/frontend/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Logo from "./imgs/logo.png"
+
+export default function Navbar() {
+
+
+  return (
+    <div className="navbar">
+      <img src={Logo} onClick={() => this.nexPath("/")}></img>
+    </div>
+  )
+}
